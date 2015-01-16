13f7a769f284e67d472c31ee51e4047b8c96505d
nathanfaucett <nathanfaucett@gmail.com>
2015-01-16 09:06:40 -0600

    moved fromIndex default value to exported function

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var isArray = require("is_array");
 
 
 function arrayIndexOf(array, value, fromIndex) {
-    var i = (fromIndex || 0) - 1,
+    var i = fromIndex - 1,
         il = array.length - 1;
 
     while (i++ < il) {
@@ -15,5 +15,5 @@ function arrayIndexOf(array, value, fromIndex) {
 }
 
 module.exports = function indexOf(array, value, fromIndex) {
-    return isArray(array) ? arrayIndexOf(array, value, fromIndex) : -1;
+    return isArray(array) ? arrayIndexOf(array, value, fromIndex || 0) : -1;
 };

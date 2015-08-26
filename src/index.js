f95784d350748dde95b2b05472b91124c10590db
nathanfaucett <nathanfaucett@gmail.com>
2015-08-26 11:08:59 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-var isEqual = require("is_equal"),
-    isLength = require("is_length"),
-    isObjectLike = require("is_object_like");
+var isEqual = require("is_equal");
 
 
 module.exports = indexOf;
 
 
 function indexOf(array, value, fromIndex) {
-    return (isObjectLike(array) && isLength(array.length)) ? arrayIndexOf(array, value, fromIndex || 0) : -1;
-}
-
-function arrayIndexOf(array, value, fromIndex) {
-    var i = fromIndex - 1,
+    var i = (fromIndex || 0) - 1,
         il = array.length - 1;
 
     while (i++ < il) {
--- test/test.js
+++ test/test.js
@@ -6,6 +6,5 @@ describe("indexOf", function() {
     it("should return index of item in array", function() {
         assert.equal(indexOf([0, 1, 2], 1), 1);
         assert.equal(indexOf([0, 1, 2], 3), -1);
-        assert.equal(indexOf(null, 3), -1);
     });
 });
